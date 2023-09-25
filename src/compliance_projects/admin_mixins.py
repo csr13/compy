311e3bc59f6ce3218c76cc9b05e211990c160130
csr13 <carsaroca.13@gmail.com>
2023-09-25 05:18:41 -0600

    <+> subControls csv export fix

--- src/compliance_projects/admin_mixins.py
+++ src/compliance_projects/admin_mixins.py
@@ -1,4 +1,5 @@
 import csv
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseRedirect
@@ -27,32 +28,39 @@ class CsvAdminMixin(object):
             app_name = meta.app_label
             controls = obj.controls.all()
             if controls.count() < 0:
-                messages.info(request, "This Framework has no controls")
+                messages.warning(request, "This Framework has no controls")
                 return HttpResponseRedirect(
                     reverse(
-                        "admin:%s_%s_change" % (
-                            model_name, 
-                            app_name
+                        "admin:%s_%s_changelist" % (
+                            app_name,
+                            model_name
                         )
                     )
                 )
-            control_fields = [x.name for x in controls[0]._meta.fields]
+            control_fields = [x.name for x in controls[0]._meta.fields] + ["subcontrols"]
             resp = HttpResponse(content_type='text/csv')
             file_name = "_".join(obj.name.split(" ")).lower() + "_controls.csv"
             resp['Content-Disposition'] = 'attachment; filename=%s' % file_name
-            writer = csv.writer(resp)
-            writer.writerow(control_fields)
+            writer = csv.DictWriter(resp, fieldnames=control_fields)
+            writer.writeheader()
             for control in controls:
-                row = writer.writerow(
-                    [getattr(control, field) for field in control_fields]
-                )
+                subcontrols = ",".join([sub.ref_code for sub in control.subcontrols.all()])
+                row = {field : getattr(control, field) for field in control_fields}
+                row["subcontrols"] = subcontrols
+                row = writer.writerow(row)
+
         except Exception as error:
-            messages.info(request, "This Framework has no controls")
+            if settings.DEBUG:
+                raise error
+                message = str(error)
+            else:
+                message = "Unable to process request for csv"
+            messages.warning(request, message)
             return HttpResponseRedirect(
                 reverse(
-                    "admin:%s_%s_change" % (
-                        model_name, 
-                        app_name
+                    "admin:%s_%s_changelist" % (
+                        app_name,
+                        model_name
                     )
                 )
             )
